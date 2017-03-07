a3ad90c0c314f03ca8ed095214b29168f56b22fa
Andrew Randall <arandall@sheetmusicplus.com>
2017-03-07 14:42:40 -0800

    Both branches combined

--- lib/active_merchant/billing/gateways/litle.rb
+++ lib/active_merchant/billing/gateways/litle.rb
@@ -50,6 +50,7 @@ module ActiveMerchant #:nodoc:
 
       def capture(money, authorization, options={})
         transaction_id, _, _ = split_authorization(authorization)
+        options[:partial] = true if money
 
         request = build_xml_request do |doc|
           add_authentication(doc)
@@ -181,7 +182,7 @@ module ActiveMerchant #:nodoc:
         add_order_source(doc, payment_method, options)
         add_billing_address(doc, payment_method, options)
         add_shipping_address(doc, payment_method, options)
-        add_payment_method(doc, payment_method)
+        add_payment_method(doc, payment_method, options)
         add_pos(doc, payment_method)
         add_descriptor(doc, options)
         add_debt_repayment(doc, options)
@@ -200,10 +201,13 @@ module ActiveMerchant #:nodoc:
         doc.debtRepayment(true) if options[:debt_repayment] == true
       end
 
-      def add_payment_method(doc, payment_method)
+      def add_payment_method(doc, payment_method, options)
         if payment_method.is_a?(String)
           doc.token do
             doc.litleToken(payment_method)
+            doc.expDate(options[:expiration_date]) if options[:expiration_date].present?
+            doc.cardValidationNum(options[:card_validation_num]) if options[:card_validation_num].present?
+            doc.type(options[:type]) if options[:type].present?
           end
         elsif payment_method.respond_to?(:track_data) && payment_method.track_data.present?
           doc.card do
@@ -338,6 +342,7 @@ module ActiveMerchant #:nodoc:
         attributes[:id] = truncate(options[:id] || options[:order_id], 24)
         attributes[:reportGroup] = options[:merchant] || 'Default Report Group'
         attributes[:customerId] = options[:customer]
+        attributes[:partial] = options[:partial]
         attributes.delete_if { |key, value| value == nil }
         attributes
       end
